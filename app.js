7e2db50f68698c72da1d9648b6d08397d79043b4
Maples7 <maples7@163.com>
2016-12-23 22:36:19 +0800

    init with echarts, webpack, showdown and yarn

---
+++ app.js
@@ -0,0 +1,15 @@
+const echarts = require('echarts/lib/echarts');
+
+// 引入提示框和标题组件
+require('echarts/lib/component/tooltip');
+require('echarts/lib/component/title');
+
+// TODO: 
+// 1. convert excel data into echarts json style
+// 2. draw charts
+// 3. beautify and debug
+
+let chart1 = echarts.init(document.getElementById('chart1'));
+chart1.setOption({
+
+});
